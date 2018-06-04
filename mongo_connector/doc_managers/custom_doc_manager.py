79c0c22eff77ba250121ce6299c61c2b26ba76cd
Nikhil Navin <nnavin@akamai.com>
2018-06-04 23:23:22 +0530

    write to neo4j graph

--- mongo_connector/doc_managers/custom_doc_manager.py
+++ mongo_connector/doc_managers/custom_doc_manager.py
@@ -2,6 +2,47 @@ from mongo_connector.doc_managers.doc_manager_base import DocManagerBase
 
 
 from mongo_connector import errors, constants
+from neo4j.v1 import GraphDatabase
+
+
+
+class Neo4jconnector(object):
+    
+    def __init__(self):
+        uri = "bolt://localhost:7687"
+        self.driver = GraphDatabase.driver(uri, auth=("neo4j", "neodev"))
+    
+
+
+    def createUser(self,user):
+        usr = dict()
+        usr['appFBId'] = user['appFBId']
+        usr['fullName'] = user["fullName"]
+        usr['mobiles']=user['mobileNumber']
+        usr['email']=user.get('email',None)
+        usr['legitId']=user.get('legit_id',None)
+        with self.driver.session() as session:
+            with session.begin_transaction() as tx:
+                tx.run("Create (u:UserNode {appFBId:{appFBId},fullName:{fullName},mobile:{mobiles},email:{email},legitId:{legitId}});",**usr)
+
+    def createPhoneContact(self,data):
+        with self.driver.session() as session:
+            with session.begin_transaction() as tx:
+                data = {'appfbid':data['appFBId']}
+                print(data)
+                tx.run("Match (u:UserNode{appFBId:{appfbid}}),(p:PhoneContact{appFBId:{appfbid}}) WHERE NOT (u.fullName = p.contactName) MERGE (u)-[:phone_contacts]->(p)",**data)
+
+    def createSameContact(self,data):
+        with self.driver.session() as session:
+            with session.begin_transaction() as tx:
+                data = {'appFBId':data['appFBId']}
+                tx.run("MATCH (n:UserNode{appFBId:{appFBId}})-[:phone_contacts]->(p:PhoneContact) with p match(p2:PhoneContact{phoneNumber:p.phoneNumber})  where not p=p2 merge (p)-[:same_contact]-(p2)",**data)
+
+    def createUserNumber(self,data):
+        with self.driver.session() as session:
+            with session.begin_transaction() as tx:
+                data = {'appFBId':data['appFBId']}
+                tx.run("match (u:UserNode{appFBId:{appFBId}}) create unique (u)-[:user_contact]->(:PhoneContact {contactName:u.fullName,phoneNumber:u.mobile,contactType:'mobile',appFBId:u.appFBId})",**data)
 
 
 class DocManager(DocManagerBase):
