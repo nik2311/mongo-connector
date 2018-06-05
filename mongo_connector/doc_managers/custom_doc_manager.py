2ed3ab8c66ec8d666226e1f7fb2809abbf78adce
Nikhil Navin <nnavin@akamai.com>
2018-06-05 18:41:46 +0530

    create user update user create contact

--- mongo_connector/doc_managers/custom_doc_manager.py
+++ mongo_connector/doc_managers/custom_doc_manager.py
@@ -9,35 +9,61 @@ from neo4j.v1 import GraphDatabase
 class Neo4jconnector(object):
     
     def __init__(self,uri="bolt://localhost:7687",auth=("neo4j", "neodev")):
-        self.driver = GraphDatabase.driver(uri, auth=auth
+        self.driver = GraphDatabase.driver(uri, auth=auth)
+
+    def createUser(self,user,namespace=None):
+        if namespace=='appusers':
+            usr = dict()
+            usr['appFBId'] = user['appFBId']
+            usr['fullName'] = user["fullName"]
+            usr['mobiles']=user['mobileNumber']
+            usr['email']=user.get('email',None)
+            usr['legitId']=user.get('legit_id',None)
+            with self.driver.session() as session:
+                with session.begin_transaction() as tx:
+                    tx.run("Create (u:UserNode {appFBId:{appFBId},fullName:{fullName},mobile:{mobiles},email:{email},legitId:{legitId}});",**usr)
+
+    def updateUser(self,user,namespace=None):
+        if namespace=='appusers':
+            usr = dict()
+            usr['appFBId'] = user['appFBId']
+            usr['fullName'] = user["fullName"]
+            usr['mobiles']=user['mobileNumber']
+            usr['email']=user.get('email',None)
+            usr['appFBId'] = user['appFBId']
+            with self.driver.session() as session:
+                with session.begin_transaction() as tx:
+                    tx.run("Match (u:UserNode {appFBId:{appFBId}}) Set u.fullName={fullName},u.mobile={mobiles},u.email={email};",**usr)
+
     
+    def createNewPhoneContact(self,data,namespace=None):
+        if namespace=='appuserphonecontacts':
+            for phonedata in data.get('contactRow',{}).get('phones',[]):
+                phone = dict()
+                phone['appFBId'] = data.get('appFBId',None)
+                if phone['appFBId'] :
+                    phone['contactName'] = data['contactRow']["fullName"]
+                    phone['phoneNumber']=phonedata['data']
+                    print(phone)
+                    with self.driver.session() as session:
+                        with session.begin_transaction() as tx:
+                            tx.run("Create (u:PhoneContact {appFBId:{appFBId},contactName:{contactName},phoneNumber:{phoneNumber}});",**phone)
+        
 
-
-    def createUser(self,user):
-        usr = dict()
-        usr['appFBId'] = user['appFBId']
-        usr['fullName'] = user["fullName"]
-        usr['mobiles']=user['mobileNumber']
-        usr['email']=user.get('email',None)
-        usr['legitId']=user.get('legit_id',None)
-        with self.driver.session() as session:
-            with session.begin_transaction() as tx:
-                tx.run("Create (u:UserNode {appFBId:{appFBId},fullName:{fullName},mobile:{mobiles},email:{email},legitId:{legitId}});",**usr)
-
-    def createPhoneContact(self,data):
+    def createPhoneContactRelation(self,data,namespace=None):
         with self.driver.session() as session:
             with session.begin_transaction() as tx:
                 data = {'appfbid':data['appFBId']}
                 print(data)
                 tx.run("Match (u:UserNode{appFBId:{appfbid}}),(p:PhoneContact{appFBId:{appfbid}}) WHERE NOT (u.fullName = p.contactName) MERGE (u)-[:phone_contacts]->(p)",**data)
 
-    def createSameContact(self,data):
+    def createSameContact(self,data,namespace=None):
         with self.driver.session() as session:
             with session.begin_transaction() as tx:
                 data = {'appFBId':data['appFBId']}
                 tx.run("MATCH (n:UserNode{appFBId:{appFBId}})-[:phone_contacts]->(p:PhoneContact) with p match(p2:PhoneContact{phoneNumber:p.phoneNumber})  where not p=p2 merge (p)-[:same_contact]-(p2)",**data)
 
-    def createUserNumber(self,data):
+    def createUserNumber(self,data,namespace=None):
         with self.driver.session() as session:
             with session.begin_transaction() as tx:
                 data = {'appFBId':data['appFBId']}
@@ -56,10 +82,13 @@ class DocManager(DocManagerBase):
         print(kwargs)
 
     def upsert(self, doc, namespace, timestamp):
-        print("INSERT :",doc)
+        print("INSERT :",doc,namespace)
+        self.neo_connect.createUser(doc,namespace=namespace.split('.')[1])
+        self.neo_connect.createNewPhoneContact(doc,namespace=namespace.split('.')[1])
 
     def update(self, document_id, update_spec, namespace, timestamp):
-        print("UPDATE :",update_spec)
+        print("UPDATE :",update_spec,namespace)
+        self.neo_connect.updateUser(update_spec,namespace=namespace.split('.')[1])
 
     def remove(self, document_id, namespace, timestamp):
         print("DELETE : ",document_id,namespace)
