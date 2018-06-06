5d831c8396df564f32a897095dc2dcd78559d549
Nikhil Navin <nnavin@akamai.com>
2018-06-06 22:15:44 +0530

    create relations

--- mongo_connector/doc_managers/custom_doc_manager.py
+++ mongo_connector/doc_managers/custom_doc_manager.py
@@ -24,6 +24,7 @@ class Neo4jconnector(object):
             usr['mobiles']=user['mobileNumber']
             usr['email']=user.get('email',None)
             usr['legitId']=user.get('legit_id',None)
+            print("CREATE USER ",usr)
             with self.driver.session() as session:
                 with session.begin_transaction() as tx:
                     tx.run("Create (u:UserNode {appFBId:{appFBId},fullName:{fullName},mobile:{mobiles},email:{email},legitId:{legitId}});",**usr)
@@ -49,7 +50,7 @@ class Neo4jconnector(object):
                 if phone['appFBId'] :
                     phone['contactName'] = data['contactRow']["fullName"]
                     phone['phoneNumber']=phonedata['data']
-                    print(phone)
+                    print("CREATE CONTACT ",phone)
                     with self.driver.session() as session:
                         with session.begin_transaction() as tx:
                             tx.run("Create (u:PhoneContact {appFBId:{appFBId},contactName:{contactName},phoneNumber:{phoneNumber}});",**phone)
@@ -61,13 +62,14 @@ class Neo4jconnector(object):
         with self.driver.session() as session:
             with session.begin_transaction() as tx:
                 data = {'appfbid':data['appFBId']}
-                print(data)
+                print("CREATE PHONE_CONTACTS REL ",data)
                 tx.run("Match (u:UserNode{appFBId:{appfbid}}),(p:PhoneContact{appFBId:{appfbid}}) WHERE NOT (u.fullName = p.contactName) MERGE (u)-[:phone_contacts]->(p)",**data)
 
     def createSameContact(self,data,namespace=None):
         with self.driver.session() as session:
             with session.begin_transaction() as tx:
                 data = {'appFBId':data['appFBId']}
+                print("CREATE SAME_CONTACTS REL ",data)
                 tx.run("MATCH (n:UserNode{appFBId:{appFBId}})-[:phone_contacts]->(p:PhoneContact) with p match(p2:PhoneContact{phoneNumber:p.phoneNumber})  where not p=p2 merge (p)-[:same_contact]-(p2)",**data)
 
     def createUserNumber(self,data,namespace=None):
@@ -89,20 +91,20 @@ class DocManager(DocManagerBase):
         print(kwargs)
 
     def upsert(self, doc, namespace, timestamp):
-        print("INSERT :",doc,namespace)
+        # print("INSERT :",doc,namespace)
         self.neo_connect.createUser(doc,namespace=namespace.split('.')[1])
         self.neo_connect.createNewPhoneContact(doc,namespace=namespace.split('.')[1])
 
     def update(self, document_id, update_spec, namespace, timestamp):
-        print("UPDATE :",update_spec,namespace)
+        # print("UPDATE :",update_spec,namespace)
         self.neo_connect.updateUser(update_spec,namespace=namespace.split('.')[1])
 
     def remove(self, document_id, namespace, timestamp):
         print("DELETE : ",document_id,namespace)
 
     def search(self, start_ts, end_ts):
-        print("search from {0} to {1}".format(start_ts,end_ts))
+        # print("search from {0} to {1}".format(start_ts,end_ts))
 
     def handle_command(self, doc, namespace, timestamp):
         db = namespace.split('.', 1)[0]
-        print(db,namespace)
+        # print(db,namespace)
